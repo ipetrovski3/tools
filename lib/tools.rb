35fe7408d14290c2d51bd90d85b8f666156dec8c
Igor Petrovski <ipetrovski3@gmail.com>
2020-03-18 11:39:45 +0100

    test done

--- lib/tools.rb
+++ lib/tools.rb
@@ -1,6 +1,74 @@
-require "tools/version"
+# frozen_string_literal: true
+
+require 'tools/version'
 
 module Tools
-  class Error < StandardError; end
-  # Your code goes here...
+  class Luhn
+    def initialize(str)
+      @str = str
+    end
+
+    def valid?
+      return false if valid_checks == false
+      return false if (sumall % 10).zero? == false
+
+      true
+    end
+
+    private
+
+    def striped
+      @str.delete(' ')
+    end
+
+    def rev_arr
+      striped.reverse.chars
+    end
+
+    def every_first_array
+      efa = (0..rev_arr.size - 1).step(2)
+      efa.map { |i| rev_arr[i] }
+    end
+
+    def every_second_array
+      esa = (1..rev_arr.size - 1).step(2)
+      esa.map { |i| rev_arr[i] }
+    end
+
+    def str_to_int1
+      every_first_array.map(&:to_i)
+    end
+
+    def str_to_int2
+      every_second_array.map(&:to_i)
+    end
+
+    def multi
+      str_to_int2.map { |i| rule(i) }
+    end
+
+    def rule(num)
+      multi = num * 2
+      if multi > 9
+        multi - 9
+      else
+        multi
+      end
+    end
+
+    def sum_all
+      str_to_int1 + multi
+    end
+
+    def sumall
+      sum_all.sum
+    end
+
+    def valid_checks
+      return false if @str.length <= 1
+      return false if @str.chars.any? { |char| ('a'..'z').include? char.downcase }
+      return false if @str.length == 2 && @str[0] == ' ' && @str[1] == '0'
+      return false if !@str.scan(/[!$&#-]/).empty? || @str.include?(':')
+    end
+  end
 end
---
+++ spec/luhn_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+RSpec.describe Tools::Luhn do
+  it "doesn't allow single digit string" do
+    luhn = Tools::Luhn.new('3')
+    expect(luhn.valid?).to be false
+  end
+
+  it "doesn't allow empty string" do
+    luhn = Tools::Luhn.new('')
+    expect(luhn.valid?).to be false
+  end
+
+  it "doesn't allow single 0 input " do
+    luhn = Tools::Luhn.new('0')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'remains valid if digits reversed' do
+    luhn = Tools::Luhn.new('059')
+    expect(luhn.valid?).to be true
+    puts luhn.valid?
+  end
+
+  it 'became invalid if digits reversed' do
+    luhn = Tools::Luhn.new('59')
+    expect(luhn.valid?).to be true
+    puts luhn.valid?
+  end
+
+  it 'is valid canadian SIN number' do
+    luhn = Tools::Luhn.new('055 444 285')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'is invalid canadian SIN number' do
+    luhn = Tools::Luhn.new('055 444 286')
+    expect(luhn.valid?).to be false
+  end
+
+  it "is invalid credit card" do
+    luhn = Tools::Luhn.new('8273 1232 7352 0569')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'becomes invalid if non digit is included' do
+    luhn = Tools::Luhn.new('055a 444 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'becomes invalid if non digit is added at the end' do
+    luhn = Tools::Luhn.new('059a')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'becomes invalid if punctations are used' do
+    luhn = Tools::Luhn.new('055-444-285')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'becomes invalid if symbols are included' do
+    luhn = Tools::Luhn.new('055£ 444$ 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it "doesn't allow single 0 with space" do
+    luhn = Tools::Luhn.new(' 0')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'is valid if more than single 0 is used' do
+    luhn = Tools::Luhn.new('0000 0')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'input digit 9 is correctly converted to oput digit 9' do
+    luhn = Tools::Luhn.new('091')
+    expect(luhn.valid?).to be true
+  end
+
+  it "doesn't allow strings with non digits" do
+    luhn = Tools::Luhn.new(':9')
+    expect(luhn.valid?).to be false
+  end
+end
