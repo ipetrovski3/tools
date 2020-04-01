64813cbefb008e05976175ba399bcfa295c96b54
Igor Petrovski <ipetrovski3@gmail.com>
2020-04-01 20:12:36 +0200

    Refactored Resistors

--- spec/resistors_spec.rb
+++ spec/resistors_spec.rb
@@ -3,30 +3,28 @@
 RSpec.describe Resistors do
   it 'gives value with two colors resistor' do
     resistor = Resistors.new(['brown', 'green'])
-    expect(resistor.res_two_value).to eq(15)
+    expect(resistor.first_two_colors).to eq(15)
   end
 
   it 'ignore the value of the third color given' do
     resistor = Resistors.new(['brown', 'green', 'violet'])
-    expect(resistor.res_two_value).to eq(15)
+    expect(resistor.first_two_colors).to eq(15)
   end
 
   it 'use the third color as a multiplier to calculate ohms' do
     resistor = Resistors.new(['brown', 'green', 'violet'])
-    expect(resistor.resistor_value).to eq(150_000_000)
-  end
-
-  it 'the fourth color will show the tolerance' do
-    resistor = Resistors.new(['brown', 'green', 'violet', 'gold'])
-    expected = '± 5%'
-    expect(resistor.resistor_tolerance).to eq(expected)
+    expect(resistor.resistor_omhs).to eq(150_000_000)
   end
 
   it 'gives the full explanation of the resistor value and tolerance' do
     resistor = Resistors.new(['brown', 'green', 'violet', 'gold'])
-    expected = 'Resistor value is 150000000 omhs, with tolerance ± 5%'
-    expect(resistor.resistor).to eq(expected)
+    expected = 'Resistor value is 150000000 ohms, with ± 5% tolerance'
+    expect(resistor.resistor_specs).to eq(expected)
   end
 
-
+  it 'default tolerance value is ± 20% when no present color' do
+    resistor = Resistors.new(['brown', 'green', 'violet'])
+    expected = 'Resistor value is 150000000 ohms, with ± 20% tolerance'
+    expect(resistor.resistor_specs).to eq(expected)
+  end
 end
