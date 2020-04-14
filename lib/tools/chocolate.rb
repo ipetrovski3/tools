bf75c7b03ef39034a5647806a0da9bf4b708ba90
Igor Petrovski <ipetrovski3@gmail.com>
2020-04-14 18:52:55 +0200

    minor changes

--- lib/tools/chocolate.rb
+++ lib/tools/chocolate.rb
@@ -6,18 +6,10 @@ module Tools
     end
 
     def fairness?
-      first_person == second_person
+      pieces_eaten(@eater_one) == pieces_eaten(@eater_two)
     end
 
     private
-
-    def first_person
-      pieces_eaten(@eater_one)
-    end
-
-    def second_person
-      pieces_eaten(@eater_two)
-    end
 
     def pieces_eaten(eater)
       eater.map { |a, b| a * b }.sum
